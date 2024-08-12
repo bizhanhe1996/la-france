c40daa1b2933e74b93f5af43832804e05708ed3b
sepehr <bizhan.he1996@gmail.com>
2024-08-12 21:32:52 +0330

    Improved Histoire Page

--- pages/histoire.vue
+++ pages/histoire.vue
@@ -12,30 +12,94 @@
       class="vertical-timeline flex flex-col justify-center items-center gap-8 my-4 relative z-[2]"
     >
       <li>
-        <div class="prepend bg-teal-200">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto fugit incidunt voluptatum eum, ad repudiandae laborum maiores exercitationem voluptas. Molestias nesciunt blanditiis ipsum recusandae ut eligendi veniam quasi quaerat voluptate, dolor nam animi soluta repellendus!</p>
-
+        <div class="prepend">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
+            fugit incidunt voluptatum eum, ad repudiandae laborum maiores
+            exercitationem voluptas. Molestias nesciunt blanditiis ipsum
+            recusandae ut eligendi veniam quasi quaerat voluptate, dolor nam
+            animi soluta repellendus!
+          </p>
         </div>
-        <span>Before Roman <br/> Empire</span>
-        <div class="append bg-green-200">append</div>
+        <span
+          >Ancient <br />
+          France</span
+        >
+        <div class="append">
+          <img
+            width="75%"
+            src="https://picsum.photos/400/200"
+            alt="ancient france"
+          />
+        </div>
       </li>
       <li>
-        <span>Roman Empire <br/> Advent</span>
+        <div class="prepend">
+          <img
+            width="75%"
+            src="https://picsum.photos/400/400"
+            alt="ancient france"
+          />
+        </div>
+        <span>
+          The Frankish <br />
+          Kingdoms <br />
+          486-987
+        </span>
+        <div class="append">
+          <div>
+
+            <p class="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, voluptatem suscipit? Delectus dicta quo distinctio nobis corrupti praesentium temporibus, voluptatem molestias adipisci eligendi aliquid facilis dolores culpa similique cupiditate iure consectetur, illo obcaecati ducimus soluta!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam beatae et iusto eaque porro?
+            </p>
+          </div>
+        </div>
       </li>
       <li>
-        <span>Roman Empire <br/> Collapse</span>
+        <span>
+          Early French <br />
+          Monarchies <br />
+          987-1453
+        </span>
       </li>
       <li>
-        <span class="!bg-red-500">Great French <br/> Revolution</span>
+        <span>
+          Pre-Revolutionary <br />
+          France <br />
+          1453-1789
+        </span>
       </li>
       <li>
-        <span>Napoleon</span>
+        <span class="!bg-red-500 w-60">
+          Revolutionary <br />
+          France <br />
+          1789-1799
+        </span>
       </li>
       <li>
-        <span>World War II</span>
+        <span
+          >Napoleon <br />
+          1799-1815
+        </span>
       </li>
       <li>
-        <span>Modern times</span>
+        <span>
+          Post-Napoleonic <br> France <br> 1815-1914
+        </span>
+      </li>
+      <li>
+        <span>
+          World War <br> I & II <br> 1914-1945
+        </span>
+      </li>
+      <li>
+        <span>
+          Modern Times <br> 1945-now
+
+        </span>
       </li>
       <hr class="border-blue-500 border-2 absolute h-full z-[1]" />
     </ul>
@@ -55,27 +119,31 @@ const bannerDetails = {
 
 <style lang="postcss">
 ul.vertical-timeline {
-  @apply bg-blue-200;
   li {
-    @apply z-[2]  relative w-full flex;
-    
+    @apply z-[2] w-full flex items-center justify-evenly;
     div.prepend {
+      @apply p-4 bg-gray-200 relative text-justify rounded-md flex items-center justify-center;
       flex-basis: 40%;
-      flex-grow: 47.5%;
+      &::after {
+        content: "";
+        clip-path: polygon(0% 0%, 100% 50%, 0% 100%);
+        @apply bg-gray-200 top-1/2 h-8 w-6 absolute -translate-y-1/2;
+        left: 98%;
+      }
     }
     span {
-      flex-basis: 5%;
-      flex-grow: 20%;
-      aspect-ratio: 1;
-      width: 100%;
-      height: 100%;
-      @apply bg-blue-500 select-none text-white text-center aspect-square px-4 py-2 rounded-full inline-flex mx-auto items-center justify-center min-w-min;
+      @apply select-none text-white bg-blue-500 p-4 font-bold aspect-square rounded-full flex items-center justify-center text-center;
     }
     div.append {
+      @apply rounded-md flex items-center justify-center relative bg-gray-200 p-4;
+      &::before {
+        @apply h-8 w-6 bg-gray-200 absolute;
+        content: "";
+        right: 98%;
+        clip-path: polygon(0% 50%, 100% 0%, 100% 100%);
+      }
       flex-basis: 40%;
-      flex-grow: 47.5%;
     }
-
   }
 }
 </style>
