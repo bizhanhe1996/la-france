1f1c44ce6533e8af7f2b9152935e6d41b34c04c2
sepehr <bizhan.he1996@gmail.com>
2024-08-17 00:49:37 +0330

    Added animation to french revolution

---
+++ components/Timeline/Image.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="timeline-image">
+    <img
+      :src="props.path"
+      :alt="props.alt"
+      draggable="false"
+    />
+    <div
+      v-if="props.title || props.subtitle"
+    >
+      <figcaption>{{ props.title }}</figcaption>
+      <small>{{ props.subtitle }}</small>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  path: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  alt: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  title: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  subtitle: {
+    type: String,
+    required: false,
+    default: null,
+  },
+});
+</script>
+
+<style lang="postcss">
+
+div.timeline-image {
+    @apply relative;
+    img {
+        @apply w-full rounded-md;
+    }
+    div {
+        @apply select-none text-center flex flex-col absolute text-white 
+            gap-4 p-4 backdrop-brightness-50 bottom-0 w-full;
+        figcaption {
+            @apply font-bold text-3xl;
+        }
+    }
+}
+
+</style>
--- pages/histoire.vue
+++ pages/histoire.vue
@@ -13,104 +13,109 @@
     >
       <li>
         <div class="prepend triangle">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto fugit
-          incidunt voluptatum eum, ad repudiandae laborum maiores exercitationem
-          voluptas. Molestias nesciunt blanditiis ipsum recusandae ut eligendi
-          veniam quasi quaerat voluptate, dolor nam animi soluta repellendus!
+          <p class="text-justify text-slate-500">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto fugit incidunt voluptatum eum, ad repudiandae laborum maiores exercitationem voluptas. Molestias nesciunt blanditiis ipsum recusandae ut eligendi veniam quasi quaerat voluptate, dolor nam animi soluta repellendus!</p>
         </div>
-        <span class="w-36"> Ancient France </span>
+        <span> Ancient France </span>
         <div class="append !p-0">
-          <img
-            class="w-full rounded-md"
-            src="/images/histoire/ancient.jpeg"
+          <timeline-image
+            path="/images/histoire/ancient.jpeg"
             alt="ancient france"
-            draggable="false"
+            title="Title here"
+            subtitle="Subtitle goes here."
           />
-          <div
-            class="select-none absolute text-center flex flex-col gap-4 transition hover:backdrop-blur-sm p-4"
-          >
-            <figcaption class="text-xl font-bold">Title</figcaption>
-            <small>Subtitle goes here.</small>
-          </div>
         </div>
       </li>
       <li>
         <div class="prepend overflow-hidden !p-0">
-          <img
-            width="100%"
-            src="/images/histoire/charlemagne.jpeg"
-            alt="ancient france"
-            draggable="false"
+          <timeline-image
+            path="/images/histoire/charlemagne.jpeg"
+            alt="frankish kingdoms"
+            title="Charlemagne The Great"
+            subtitle="Subtitle goes here."
           />
-          <div class="absolute flex flex-col text-center gap-4 transition hover:backdrop-blur-sm p-4 select-none">
-            <figcaption class="font-bold">Charlemagne The Great</figcaption>
-            <small>subtitle goes here.</small>
-          </div>
         </div>
-        <span class="w-36"> The Frankish Kingdoms 486-987 </span>
+        <span> The Frankish Kingdoms <small>486-987</small> </span>
         <div class="append triangle">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident,
-          voluptatem suscipit? Delectus dicta quo distinctio nobis corrupti
-          praesentium temporibus, voluptatem molestias adipisci eligendi aliquid
-          facilis dolores culpa similique cupiditate iure consectetur, illo
-          obcaecati ducimus soluta!
+          <p class="text-justify text-slate-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, voluptatem suscipit? Delectus dicta quo distinctio nobis corrupti praesentium temporibus, voluptatem molestias adipisci eligendi aliquid facilis dolores culpa similique cupiditate iure consectetur, illo obcaecati ducimus soluta!</p>
         </div>
       </li>
       <li>
         <div class="prepend triangle">
-          <ul>
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio distinctio unde obcaecati! Amet maiores ut esse ea cumque quos vel fugit dignissimos reprehenderit repellendus magni, deserunt dolorem corporis voluptate accusantium?</li>
-            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At labore dolore provident ut aut, quaerat quam, rem sed enim optio eum, quas beatae sapiente assumenda necessitatibus sunt obcaecati et excepturi!</li>
-            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, veniam exercitationem alias odit sint modi quibusdam velit dignissimos nisi, sunt saepe! Ullam, voluptas. Ratione recusandae consequatur, nulla earum sint quae!</li>
+          <ul class="list-inside">
+            <li class="mb-2">
+              <b>Merovingian Dynasty</b> 
+              <p class="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio distinctio unde obcaecati! Amet maiores ut esse ea cumque quos vel fugit dignissimos reprehenderit repellendus magni, deserunt dolorem corporis voluptate accusantium?</p>
+            </li>
+            <li class="mb-2">
+              <b>Carolingian Dynasty</b> 
+              <p class="text-slate-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At labore dolore provident ut aut, quaerat quam, rem sed enim optio eum, quas beatae sapiente assumenda necessitatibus sunt obcaecati et excepturi!</p>
+            </li>
+            <li>
+              <b>Capetian Dynasty</b>
+              <p class="text-slate-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, veniam exercitationem alias odit sint modi quibusdam velit dignissimos nisi, sunt saepe! Ullam, voluptas. Ratione recusandae consequatur, nulla earum sint quae!</p>
+            </li>
           </ul>
         </div>
-        <span class="w-36">
-          Early French Monarchies 987-1453
+        <span>
+          <text>Early French Monarchies</text>
+          <small>987-1453</small>
         </span>
         <div class="append !p-0">
-          <img
-            class="w-full rounded-md"
-            src="/images/histoire/early-french-monarchies.jpg"
-            alt="ancient france"
-            draggable="false"
+          <timeline-image
+            path="/images/histoire/early-french-monarchies.jpg"
+            alt="early french monarchies"
+            title="Title goes here"
+            subtitle="Subtitle goes here."
           />
-          <div
-            class="select-none absolute text-center flex flex-col gap-4 transition hover:backdrop-blur-sm p-4"
-          >
-            <figcaption class="text-xl font-bold">Title</figcaption>
-            <small>Subtitle goes here.</small>
-          </div>
         </div>
       </li>
       <li>
+        <div class="prepend !p-0 !bg-transparent !shadow-none">
+          <timeline-image
+            path="/images/histoire/louie-xiv.jpg"
+            alt="Louie XIV"
+            title="Louie XIV"
+            subtitle="Subtitle goes here."
+          />
+        </div>
         <span class="!bg-orange-500 w-36">
-          Pre-Revolutionary France 1453-1789
+          <text>Pre-Revolutionary France</text> <small>1453-1789</small>
+        </span>
+        <div class="append triangle">
+          <ul class="list-inside list-dic">
+            <li class="mb-2">
+              <b>Social Structure</b>
+              <p class="text-slate-500 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum ducimus numquam dolor ad tenetur consectetur voluptatum distinctio vitae fugiat totam blanditiis, molestias delectus maxime amet dolorem a exercitationem eaque!</p>
+            </li>
+            <li class="mb-2">
+              <b>Political Structure</b> 
+              <p class="text-slate-500 text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem voluptate quasi quam sunt temporibus vero enim rem sequi doloribus, quos ea et consectetur, voluptas nisi tenetur a labore quas, architecto laboriosam. Nostrum officiis quod odio qui nesciunt consequatur unde excepturi?</p>
+            </li>
+            <li>
+              <b>Economic Conditions</b> 
+              <p class="text-slate-500 text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit modi dolore odio asperiores, iusto vel quod laboriosam illum a qui corrupti. Fuga voluptatem, culpa quisquam voluptas nisi hic architecto iusto quasi exercitationem esse optio. Itaque maxime sapiente placeat voluptas repellat.</p>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <span class="!bg-red-500 !w-48 animate-beat">
+          Revolutionary France <small>1789-1799</small>
         </span>
       </li>
       <li>
-        <span class="!bg-red-500 w-48">
-          Revolutionary France 1789-1799
+        <span> Napoleon <small>1799-1815</small> </span>
+      </li>
+      <li>
+        <span>
+          Post-Napoleonic France <small>1815-1914</small>
         </span>
       </li>
       <li>
-        <span class="w-36"> Napoleon 1799-1815 </span>
+        <span> World War I & II <small>1914-1945</small> </span>
       </li>
       <li>
-        <span class="w-36">
-          Post-Napoleonic France 1815-1914
-        </span>
-      </li>
-      <li>
-        <span class="w-36">
-          World War
-          I & II
-          1914-1945
-        </span>
-      </li>
-      <li>
-        <span class="w-36">
-          Modern Times 1945-now
-        </span>
+        <span> Modern Times 1945-now </span>
       </li>
       <hr class="border-blue-500 border-2 absolute h-full z-[1]" />
     </ul>
@@ -130,7 +135,7 @@ const bannerDetails = {
 
 <style lang="postcss">
 ul.vertical-timeline {
-  li {
+  & > li {
     @apply z-[2] w-full flex items-center justify-evenly;
     div.prepend {
       @apply p-4 bg-gray-200 shadow-lg shadow-zinc-300 relative text-justify rounded-md flex items-center justify-center;
@@ -142,8 +147,12 @@ ul.vertical-timeline {
         left: 98%;
       }
     }
-    span {
-      @apply select-none text-white bg-blue-500 p-4 font-bold aspect-square rounded-full flex items-center justify-center text-center;
+    & > span {
+      @apply select-none text-white bg-blue-500 p-4 font-bold aspect-square rounded-full flex flex-col items-center justify-center w-36 text-center;
+      small {
+        font-size: 0.8rem;
+        font-weight: normal;
+      }
     }
     div.append {
       @apply rounded-md shadow-lg shadow-zinc-300 flex items-center justify-center relative bg-gray-200 p-4;
@@ -157,4 +166,27 @@ ul.vertical-timeline {
     }
   }
 }
+</style>
+
+<style>
+
+@keyframes animation-beat {
+  from {
+    filter:drop-shadow(0 0 0 red);
+    transform: scale(1);
+  }
+  to {
+    filter:drop-shadow(0 0 1rem red);
+    transform: scale(1.1);
+  }
+}
+
+.animate-beat {
+  animation-name: animation-beat;
+  animation-duration: 1s;
+  animation-timing-function: ease;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
 </style>
